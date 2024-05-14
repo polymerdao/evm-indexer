ac1d91220b38c75e12d34e637d753d212023afd5
Inkvi <alex@polymerlabs.org>
2024-05-14 11:52:39 -0700

    Add generated stat model

---
+++ src/model/generated/stat.model.ts
@@ -0,0 +1,20 @@
+import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, StringColumn as StringColumn_, IntColumn as IntColumn_} from "@subsquid/typeorm-store"
+
+@Entity_()
+export class Stat {
+    constructor(props?: Partial<Stat>) {
+        Object.assign(this, props)
+    }
+
+    @PrimaryColumn_()
+    id!: string
+
+    @StringColumn_({nullable: false})
+    name!: string
+
+    @IntColumn_({nullable: false})
+    val!: number
+
+    @IntColumn_({nullable: false})
+    chainId!: number
+}
