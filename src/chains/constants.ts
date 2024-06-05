055426d3593c2f91507eb2551e341a73f3b8f724
Inkvi <alex@polymerlabs.org>
2024-06-04 17:18:20 -0700

    Calculate missing packet metrics

--- src/chains/constants.ts
+++ src/chains/constants.ts
@@ -1,2 +1,3 @@
 export const VERSION = process.env.VERSION ?? '1';
 export const MAX_BATCH_CALL_SIZE = Number(process.env.MAX_BATCH_CALL_SIZE ?? "100")
+export const CATCHUP_BATCH_SIZE = Number(process.env.CATCHUP_BATCH_SIZE ?? "10")
