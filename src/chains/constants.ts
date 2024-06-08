8420d53f12b15cd3772db53996f01a6e62766e22
Inkvi <alex@polymerlabs.org>
2024-06-07 20:58:15 -0700

    Calculate packet metrics concurrently

--- src/chains/constants.ts
+++ src/chains/constants.ts
@@ -3,3 +3,4 @@ export const MAX_BATCH_CALL_SIZE = Number(process.env.MAX_BATCH_CALL_SIZE ?? "10
 export const CATCHUP_BATCH_SIZE = Number(process.env.CATCHUP_BATCH_SIZE ?? "10")
 export const ENABLE_CATCHUP = process.env.ENABLE_CATCHUP === 'true'
 export const CATCHUP_ERROR_LIMIT = Number(process.env.CATCHUP_ERROR_LIMIT ?? "3")
+export const BACKFILL_CONCURRENCY = Number(process.env.BACKFILL_CONCURRENCY ?? "10")
