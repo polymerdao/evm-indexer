a0ddb6a8023a542f2116fc067c530c8394ef8fc2
mvpoyatt <mpoyatt@gmail.com>
2024-06-18 17:47:47 -0400

    Fixed PR comments

--- src/handlers/channels.ts
+++ src/handlers/channels.ts
@@ -452,7 +452,6 @@ export async function channelMetrics(channelIds: string[], ctx: Context): Promis
         await updateInitToAckMetrics(channel, ctx);
         ackChannels.set(channel.channelOpenAck.id, channel.channelOpenAck);
       } catch (e) {
-        console.log(e)
         ctx.log.error(`Error updating init to ack metrics for channel ${channel.id}: ${e}`)
         channel.catchupError!.initToAckPolymerGas += 1
         catchupErrors.add(channel.catchupError!);
